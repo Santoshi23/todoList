dc5757780aa775419ec960f12b43a4c0f8acafe2
Santoshi23 <santoshi09315@gmail.com>
2023-10-06 17:55:19 +0530

    third commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,3 @@
-// import React from 'react'
-
-// function Todolist(props) {
-//   return (
-//     <li className='list-item'>
-//         <div className='item-box'>
-//             {props.item}
-//             <span className='icons'>
-//             <i className="fa-solid fa-trash-can icon-delete"
-//             onClick={e=>{
-//                 props.deleteItem(props.index)
-//             }}>
-//             </i>
-//             </span>
-//         </div>
-//     </li>
-//   );
-// }
-
-// export default Todolist
-
-
 import React from 'react';
 
 function Todolist(props) {
